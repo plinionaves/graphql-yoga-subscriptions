97a0013460141702c2f52590fa8b9fdd9c6da540
Plinio Naves <pliniopjn@hotmail.com>
2019-06-24 19:04:58 -0300

    [fix] changed post id type

--- src/mocks/posts.js
+++ src/mocks/posts.js
@@ -1,6 +1,6 @@
 const POSTS = [
-  { id: 1, title: 'First post', author: 'Plínio Naves' },
-  { id: 2, title: 'Second post', author: 'Plínio Naves' }
+  { id: '1', title: 'First post', author: 'Plínio Naves' },
+  { id: '2', title: 'Second post', author: 'Plínio Naves' }
 ]
 
 module.exports = {
